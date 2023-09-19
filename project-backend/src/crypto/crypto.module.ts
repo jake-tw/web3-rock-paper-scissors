472a1aaedac4ab13b1dfb7d12c0abe0ba4e10b99
jake.lin <56834723+jake-tw@users.noreply.github.com>
2023-09-19 18:46:34 +0800

    Add config module to crypto module

--- project-backend/src/crypto/crypto.module.ts
+++ project-backend/src/crypto/crypto.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CryptoService } from './crypto.service';
 import { CryptoScheduler } from './crypto.scheduler';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   providers: [CryptoService, CryptoScheduler],
   exports: [CryptoService],
 })
